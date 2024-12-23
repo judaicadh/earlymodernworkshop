b6e06887b335bc9e75a9de87b671cbe25d518073
judaicadhpenn <157063908+judaicadhpenn@users.noreply.github.com>
2024-12-23 14:53:02 -0500

    added a new site

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js',
+		'./public/pagefind/**/*.html', // Include Pagefind outputs if needed
+	],
+
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#1D4ED8',
+				secondary: '#9333EA',
+				accent: '#FACC15', // Add custom colors
+			},
+			spacing: {
+				'72': '18rem',
+				'84': '21rem',
+				'96': '24rem', // Add custom spacing
+			},
+		},
 	},
+
 	plugins: [
 		require('@tailwindcss/typography'),
-
+		require('@tailwindcss/forms'), // Optional form plugin
 		require('flowbite/plugin'),
 	],
 };
