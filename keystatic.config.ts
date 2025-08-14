ecf60c92f8d9af2b9e1eccdb0c3b581ad3ac0331
judaicadhpenn <157063908+judaicadhpenn@users.noreply.github.com>
2025-08-14 15:35:01 -0400

    Update keystatic.config.ts

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -8,7 +8,7 @@ export default config({
     collections: {
         documents: collection({
             label: 'Documents',
-            path: 'src/content/documents/{year}/*.md',
+            path: 'src/content/documents/{year}/*',
             slugField: 'slug',
             format: { contentField: 'content' },
             schema: {
