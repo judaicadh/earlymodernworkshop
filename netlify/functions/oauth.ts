e7b4b13414d3e7c3e0f9c168795d4b3671fc9c34
judaicadhpenn <157063908+judaicadhpenn@users.noreply.github.com>
2025-08-14 12:55:46 -0400

    deleted oauth

--- netlify/functions/oauth.ts
+++
@@ -1,8 +0,0 @@
-import type { Handler } from '@netlify/functions'
-import { createNetlifyAuth } from 'netlify-cms-oauth-provider-node'
-
-const provider = createNetlifyAuth({ provider: 'github' })
-
-export const handler: Handler = async (event, context) => {
-    return provider.handler(event, context)
-}
