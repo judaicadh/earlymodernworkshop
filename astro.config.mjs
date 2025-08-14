5a56dd210636fe8134d02b2addd90d923b494211
judaicadhpenn <157063908+judaicadhpenn@users.noreply.github.com>
2025-08-14 12:45:01 -0400

    added oauth

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,38 +4,31 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import pagefind from "astro-pagefind";
-
 import markdoc from '@astrojs/markdoc';
-
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 import { rehypeHeadingIds } from '@astrojs/markdown-remark';
-
 import partytown from '@astrojs/partytown';
-
 import collectionSearch from 'astro-collection-search';
 import citePlugin from "@benrbray/remark-cite";
 import readingTime from 'astro-reading-time';
-
 import itsmatteomanfpagefind from '@itsmatteomanf/astro-pagefind';
 import remarkToc from "remark-toc";
-
 import remarkGfm from "remark-gfm";
 import rehypeMeta from "rehype-meta";
-
 import remarkLint from "remark-lint";
+import decapOauth from 'astro-decap-cms-oauth';
+
 import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
-
-
 import netlify from '@astrojs/netlify';
+
 
 
 // https://astro.build/config
 export default defineConfig({
   // The full URL to your site
   site: 'https://earlymodernworkshop.judaicadhpenn.org',
-
+  output: 'server',
   markdown: {
     remarkPlugins: [
        [ remarkToc, { heading: 'toc', maxDepth: 3 }],
@@ -65,7 +58,8 @@ export default defineConfig({
     partytown(),
     collectionSearch(),
     readingTime(),
-    itsmatteomanfpagefind()
+    itsmatteomanfpagefind(),
+    decapOauth(),
   ],
 
   adapter: netlify()
--- netlify/functions/oauth.ts
+++ netlify/functions/oauth.ts
@@ -1,11 +1,7 @@
 import type { Handler } from '@netlify/functions'
 import { createNetlifyAuth } from 'netlify-cms-oauth-provider-node'
 
-// Map CMS endpoints to the provider (GitHub)
-const provider = createNetlifyAuth({
-    provider: 'github',
-    // Optional: restrict to your org with allowList, etc.
-})
+const provider = createNetlifyAuth({ provider: 'github' })
 
 export const handler: Handler = async (event, context) => {
     return provider.handler(event, context)
